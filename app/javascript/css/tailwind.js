2e4b7568bd965ca18911d10bc220e3cab4d1256f
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-07-08 02:51:56 +0200

    implement user sign-up; stylize sign-up page

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+      colors: {
+        main: {
+          // light: '#85d7ff',
+          DEFAULT: '#60b0e2',
+          dark: '#41a1dd',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,40 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string           not null
-#  first_name :string           not null
-#  is_public  :boolean          default(TRUE), not null
-#  last_name  :string
-#  username   :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string           not null
+#  is_public              :boolean          default(TRUE), not null
+#  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  username               :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 # Indexes
 #
-#  index_users_on_email     (email) UNIQUE
-#  index_users_on_username  (username) UNIQUE
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   before_save :ensure_proper_name_case
 
   validates :email, uniqueness: true
