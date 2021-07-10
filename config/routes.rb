c48fdd7cfe069c703995e84f64cfdb2fa941598b
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-07-10 13:33:23 +0200

    generate timelines controller & prepare router

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
   end
   devise_for :users
+  authenticate :user do
+    resources :timelines, only: %i[index show]
+  end
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
