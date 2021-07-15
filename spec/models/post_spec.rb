e080e4f92a3a9bb017d9f1f14802adb2ef1cc81e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-07-15 05:25:14 +0200

    fix faling specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -24,11 +24,7 @@ require "rails_helper"
 RSpec.describe Post, type: :model do
   describe "#save" do
     it "belongs to a user" do
-      user = User.create!(
-        first_name: "Blessed",
-        email: "blessed@example.com",
-        username: "blessed"
-      )
+      user = create(:user)
       post = Post.new(postable: Status.new(text: "Whohoo!"))
       post.user = user
       post.save
