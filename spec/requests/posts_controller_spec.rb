65133e62d9001120138ccb6631b87e39288f29ff
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-07-21 00:10:40 +0200

    fix spec

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,22 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe PostsController do
-  let(:user) { create :user}
-  before {sign_in user}
-  describe 'POST /posts' do
+  let(:user) { create :user }
+  before { sign_in user }
+  describe "POST /posts" do
     context "when posting a status update" do
-      it 'can post the status if all data is valid' do
+      it "can post the status if all data is valid" do
         expect {
-          post '/posts', params: { post: {
-            postable_type: 'Status',
-            status_text: 'Hello friends!'
-          } }
+          post "/posts", params: { post: {
+                      postable_type: "Status",
+                      status_text: "Hello friends!",
+                    } }
         }.to change {
           user.posts.reload.count
         }.from(0).to(1)
-        expect(response).to redirect_to timelines_path
+        expect(response).to redirect_to timelines_path(anchor: "post_#{Post.last.id}")
         follow_redirect!
-        expect(response.body).to include 'Hello friends!'
+        expect(response.body).to include "Hello friends!"
       end
     end
   end
