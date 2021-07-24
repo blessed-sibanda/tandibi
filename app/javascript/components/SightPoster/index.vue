2b681ae608c49dc427b689cedfde00448e18431e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-07-24 07:23:58 +0200

    implement form post-type switchers

---
+++ app/javascript/components/SightPoster/index.vue
@@ -0,0 +1,17 @@
+<template> </template>
+
+<script>
+import Map from "./Map.vue";
+import PlaceFinder from "./PlaceFinder.vue";
+import PlaceList from "./PlaceList.vue";
+export default {
+  components: { Map, PlaceFinder, PlaceList },
+};
+</script>
+
+<style scoped>
+.spaced {
+  @apply mt-1 mb-1;
+  font-size: 0;
+}
+</style>
