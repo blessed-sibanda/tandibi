417b91e704c1be049fdb7f2d0f42a872521735e4
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-07-24 16:32:17 +0200

    create profile-header partial

--- app/rules/user_rules.rb
+++ app/rules/user_rules.rb
@@ -8,4 +8,21 @@ class UserRules < Bali::Rules
   role :admin do
     can :see_timeline
   end
+
+  can :follow do |user, current_user|
+    user != current_user &&
+      !user.followings.include?(current_user) &&
+      !user.inward_bonds.where(
+        user: current_user,
+        state: Bond::REQUESTING,
+      ).exists?
+  end
+
+  can :unfollow do |user, current_user|
+    user != current_user &&
+      current_user.bonds.where(
+        friend: user,
+        state: [Bond::FOLLOWING, Bond::REQUESTING],
+      ).exists?
+  end
 end
